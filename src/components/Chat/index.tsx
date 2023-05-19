c3b8699af311502eae002235e123d18666d94e22
AlekseiHead <head.dev22@gmail.com>
2023-05-19 17:32:37 +0300

    create dummy

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,13 +1,18 @@
+import { ChatPanel } from '../ChatPanel';
+import { Conversation } from '../Conversation';
+
 type ChatProps = {
-  tel: string,
+  tel: string;
 };
 
 export const Chat = ({ tel }: ChatProps) => {
   return (
     <section className="chat">
       <header className="chat__header">
-        <h3>{tel}</h3>
+        <h3>+{tel}</h3>
       </header>
+      <Conversation tel={tel} />
+      <ChatPanel tel={tel} />
     </section>
   );
 };
